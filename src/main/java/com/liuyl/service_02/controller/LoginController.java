73f8738e5fcde0844f69a7d425b5c57ffc9aab73
liuyl <2099602858@qq.com>
2021-04-27 21:44:23 +0800

    111

---
+++ src/main/java/com/liuyl/service_02/controller/LoginController.java
@@ -0,0 +1,51 @@
+package com.liuyl.service_02.controller;
+
+
+import com.liuyl.service_02.config.JwtTokenUtils;
+import com.liuyl.service_02.service.LoginService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth")
+public class LoginController {
+
+
+    private final Logger log = LoggerFactory.getLogger(LoginController.class);
+
+    private final JwtTokenUtils jwtTokenUtils;
+    private final LoginService loginService;
+
+    public LoginController(JwtTokenUtils jwtTokenUtils, LoginService loginService) {
+        this.jwtTokenUtils = jwtTokenUtils;
+        this.loginService = loginService;
+    }
+
+    @RequestMapping("/login")
+    public String login(String username,String password){
+        Map map = new HashMap();
+        map.put("user",username);
+        map.put("password",password);
+        return jwtTokenUtils.createToken(map);
+    }
+
+    @GetMapping("/wx-login")
+    public Map<String,String> wxLogin(String code){
+        Map<String,String> result = new HashMap<>();
+        log.info("微信小程序登录");
+        try{
+            result = loginService.jscode2session(code);
+        }catch (Exception e){
+            e.printStackTrace();
+            log.info("微信小程序登录异常");
+        }
+        return result;
+    }
+
+}
